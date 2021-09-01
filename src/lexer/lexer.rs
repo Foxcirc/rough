af39880cebf3ff4b4a2b967c862b03ef4ae93868
foxcirc <whyfoxcirc@gmx.de>
2021-09-01 13:22:05 +0200

    ...

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -98,7 +98,7 @@ impl <'b>Lexer<'b> {
 
             // Get the next character. This uses peek, since the end of one Token
             // may be the start of another one.
-            self.current = match self.branch.next() {
+            self.current = match self.text.next() {
                 Some(v) => v,
                 None => return None
             };
