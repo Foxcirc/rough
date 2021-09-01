4eacab6c05f04c1a855f866af48bc22f919a3b6f
foxcirc <whyfoxcirc@gmx.de>
2021-09-01 14:28:52 +0200

    ...

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -83,7 +83,7 @@ impl <'b>Lexer<'b> {
         //* This is probably a somewhat inperformant solution.
         //* ... it is elegant though, at least from my perspective
         
-        println!("Next start with ",);
+        println!("Next start with {:?}",);
 
         // Skip over spaces and tabs, encountered while there's no matching going on.
         while matches!(self.text.next().unwrap(), ' ' | '\t') {};
