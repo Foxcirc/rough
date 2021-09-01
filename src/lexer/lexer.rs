6a9ebdec909f958827f15100bb50990ed5d46785
foxcirc <whyfoxcirc@gmx.de>
2021-09-01 12:29:38 +0200

    ...

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -104,7 +104,7 @@ impl <'b>Lexer<'b> {
             };
             // println!("{:?}", self.current);
             
-            if self.current == '_' { //* remove
+            if self.current == '1' { //* remove
                 let _x = 1;
             }
             // // Advance the cursor, the line only if the character is a newline.
