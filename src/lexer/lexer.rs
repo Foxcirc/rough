b43e54759e7a93cbc2010ca2c4cbfba1725823e2
foxcirc <whyfoxcirc@gmx.de>
2021-09-01 13:07:33 +0200

    ...

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -156,10 +156,7 @@ impl <'b>Lexer<'b> {
             let set = self.possible.set();
             
             // There is only one possibility for the kind of token left.
-            if set == 1 {
-                println!("{:?}", self.current);
-                break;
-            }
+            if set == 1 { break; }
 
             // There are no possible kinds of token left. This should never happen.
             else if set == 0 { panic!("Lexer: Invalid sequence at '{}', could not match to Token.", self.current) }
