8e12cfac9adf75f0fdd5a20bf60d9d71d6057fc9
foxcirc <whyfoxcirc@gmx.de>
2021-09-05 18:17:56 +0200

    ...

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -4,6 +4,7 @@ use std::iter::Peekable;
 use crate::lexer::token::{stream::TokenStream, token::{Token, TokenKind}};
 use crate::lexer::{possible::Possible, check::Check};
 use crate::lexer::pos::Pos;
+use crate::tick::Tick;
 // use super::error::{Error, ErrorKind, Traceback, Log};
 
 // /// Used to en/disable error logging inside the lexer
