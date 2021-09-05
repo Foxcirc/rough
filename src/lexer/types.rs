2348937ceca226e154b08371e38289a1b3ff1780
foxcirc <whyfoxcirc@gmx.de>
2021-09-05 18:11:30 +0200

    ...

---
+++ src/lexer/types.rs
@@ -0,0 +1,9 @@
+
+pub(crate) struct Pos {
+    /// The current column.
+    pub(crate) column: usize,
+    /// The current line.
+    pub(crate) line: usize,
+    /// All "columns"
+    pub(crate) all: usize
+}
