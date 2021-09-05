9f136ff38ee5015a2e968659882dbddea264cb12
foxcirc <whyfoxcirc@gmx.de>
2021-09-05 17:34:17 +0200

    ...

--- src/lexer/check.rs
+++ src/lexer/check.rs
@@ -16,7 +16,7 @@ impl Check for Possible {
     fn update(&mut self, chr: char, _prev: char) {
 
         /*
-            self[TokenKind::Something] = valid!{ // TODO Make this macro. (or just a function XD)
+            self[TokenKind::Something] = valid!{ // TODO Make this macro. (or just a function xDD)
                 valid('a' | 'b' | 'c'); // what chars are valid
                 previous('1' | '2'); // what previous chars are valid
                 set(TokenKind::Other & TokenKind::What) // what flags have to be already set
