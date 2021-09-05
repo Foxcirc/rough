4e92ac59d8f062da3e09b326f6496f29d6d87c59
foxcirc <whyfoxcirc@gmx.de>
2021-09-05 18:04:36 +0200

    ...

--- src/lexer/tick.rs
+++ src/lexer/tick.rs
@@ -3,10 +3,12 @@ use std::iter::Peekable;
 use std::str::Chars;
 use crate::lexer::types::Pos;
 
-/// Increment the `Pos` and return the next element.
+/// Increment the `Pos` and return the next element of self.
 pub(crate) trait Tick {
 
-    fn tick(&mut self, counter: &mut )
+    fn tick(&mut self, counter: &mut Pos) {
+
+    }
 
 }
 
